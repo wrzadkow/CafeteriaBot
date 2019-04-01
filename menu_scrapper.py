7fcefd5734a7250d84b7e5d5f7738223223ab581
Wojciech Rzadkowski <wojciech.rzadkowski@gmail.com>
2019-04-01 13:58:27 +0200

    Added whitespaces for menu readability

--- menu_scrapper.py
+++ menu_scrapper.py
@@ -4,6 +4,13 @@ from urllib.request import urlopen
 weekdays=['Monday','Tuesday','Wednesday','Thursday','Friday']
 ist_menu_url = 'http://screens.app.ist.ac.at/menu_weekly'
 
+#removes a line 'line' from a string 'string'
+def RemoveGivenLine(string,line):
+    lines = string.splitlines()
+    lines.pop(line)
+    string_joined_again = "\n".join(lines)
+    return string_joined_again
+
 def GetMenu(menu_url):
     page = urlopen(menu_url)
     soup = BeautifulSoup(page, 'html.parser')
@@ -18,10 +25,11 @@ def GetMenu(menu_url):
      [66,67,70,73,76,79,82],
      [85,86,89,92,95,98,101]]
 
-    all_day_list=[] #stores
+    all_day_list=[]
     for j in range(len(weekdays)):
-        day_string = "\n".join([lines[i]for i in days_in_menu[j]]) #string corresponding to menu for a given weekday
+        day_string = "\n\n".join([lines[i] for i in days_in_menu[j]]) #string corresponding to menu for a given weekday
         day_string = day_string.replace("\t","")
+        day_string = RemoveGivenLine(day_string,3) #remove the (whitespace b/w soups) for a nicer look
         all_day_list.append(day_string)
         
     return all_day_list
