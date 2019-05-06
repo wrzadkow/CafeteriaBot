dd8ba182f98bca2b0eed29a896e489a7af2c4092
Wojciech Rzadkowski <wojciech.rzadkowski@gmail.com>
2019-05-06 22:11:31 +0200

    Ignore the foodtrucks when scrappin

--- menu_scrapper.py
+++ menu_scrapper.py
@@ -14,26 +14,16 @@ def RemoveGivenLine(string,line):
 def GetMenu(menu_url):
     page = urlopen(menu_url)
     soup = BeautifulSoup(page, 'html.parser')
-    string=soup.get_text()
-    lines=string.splitlines()
-    lines = list(filter(None, lines))
-
-    #stores lines that correspond to [monday,tuesday,...,friday] respectively
-    days_in_menu = [[9,10,13,16,19,22,25],
-     [28,29,32,35,38,41,44],
-     [47,48,51,54,57,60,63],
-     [66,67,70,73,76,79,82],
-     [85,86,89,92,95,98,101]]
-
-    all_day_list=[]
-    for j in range(len(weekdays)):
-        day_string = "\n\n".join([lines[i] for i in days_in_menu[j]]) #string corresponding to menu for a given weekday
-        day_string = day_string.replace("\t","")
-        day_string = RemoveGivenLine(day_string,3) #remove the (whitespace b/w soups) for a nicer look
-        all_day_list.append(day_string)
-        
-    return all_day_list
-
+    table = soup.find( "table")#, {"title":"TheTitle"} )
+    rows = table.findAll("tr")
+    texts = [row.get_text().splitlines() for row in rows]
+    texts.pop(0) #remove 1st row (table header)
+    meaningful_lines = [2,6,10,16,20,24,30] #lines corresponding to food items in each day
+    days = [[text[i].replace("\t","") for i in meaningful_lines] for text in texts]
+    day_strings = ["\n\n".join(day) for day in days]
+    #remove the (whitespace b/w soups) for a nicer look
+    day_strings = [RemoveGivenLine(day_string,3) for day_string in day_strings]    
+    return day_strings
 
 day_menus = GetMenu(ist_menu_url)
 
